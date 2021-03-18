7a3c3ec2cb98657806dd6d3f7fda154b18db60d6
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-03-19 00:36:08 +0700

    streamline method to get the day, start to-do list, set up post method for user input

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
 
   var today = new Date();
-  var daysInWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  var options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
 
   res.render("index", {
-    dayOfWeek: daysInWeek[today.getDay()],
+    kindOfDay: today.toLocaleDateString("en-US", options),
   });
 
 });
+
+app.post("/", function(req, res) {
+  console.log(req.body.newItem);
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
