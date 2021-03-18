d70c38a9ab1fab71875f4a29c5bc2134b9045cf1
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-03-18 12:53:54 +0700

    initialise ejs as view engine, set up simple logic to respond to user server request with HTML file containing day of the week

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
 
   var today = new Date();
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay! It's the weekend!");
-  } else {
-    res.sendFile(__dirname + "/index.html");
-  }
+  var daysInWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  res.render("index", {
+    dayOfWeek: daysInWeek[today.getDay()],
+  });
 
 });
 
