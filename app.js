b9f041e5d42feaa58089678598e4b205834590f7
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-03-19 13:32:23 +0700

    post route pushes user submitted new item to array and redirects to route, for loop in ejs file adds new list item for each user submitted task

--- app.js
+++ app.js
@@ -7,20 +7,29 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+var items = [];
+
 app.get("/", function(req, res) {
 
   var today = new Date();
   var options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
 
+  var day = today.toLocaleString("en-US", options);
+
   res.render("index", {
-    kindOfDay: today.toLocaleDateString("en-US", options),
-  });
+    kindOfDay: day, newListItems: items
+  }
+);
 
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body.newItem);
-})
+
+  items.push(req.body.newItem);
+
+  res.redirect("/");
+
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
