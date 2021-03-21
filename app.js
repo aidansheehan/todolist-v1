d60362d940277618b3aef8edd08887b0390bc4af
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-03-21 21:48:29 +0700

    pass date to index.ejs, change colour based on weekend or weekday

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  res.render("index");
+
+  var today = new Date();
+  const options = { weekday: "long" }
+
+  var day = today.toLocaleString("en-US", options);
+
+  res.render("index", {
+    kindOfDay: day}
+  );
 
 })
 
