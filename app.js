60ece2d141b7471e11a6c77a4b0285865507fc06
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-03-21 22:04:23 +0700

    successful rebuild - post form value from user to server, then send back and add to list

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function(req, res) {
   var today = new Date();
   const options = { weekday: "long" }
 
   var day = today.toLocaleString("en-US", options);
 
   res.render("index", {
-    kindOfDay: day}
+    kindOfDay: day, listItems: items }
   );
+})
 
+var items = [];
+
+app.post("/", function(req, res) {
+  items.push(req.body.newItem);
+  console.log(items);
+  res.redirect("/");
 })
 
 app.listen(3000, function() {
